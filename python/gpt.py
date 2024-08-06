afcfe44f965873cd0f2fde2cb6189918a360a937
albinhasanaj <albinhasanaj06@hotmail.com>
2024-08-06 07:51:42 +0200

    changes for oliver to see

--- python/gpt.py
+++ python/gpt.py
@@ -3,7 +3,10 @@ from gpt4all import GPT4All
 
 question = sys.argv[1]
 
-# model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-model = GPT4All("gpt4all-falcon-newbpe-q4_0.gguf") #fastest model
-output = model.generate(f"{question}", max_tokens=200)
-print(output, end="", flush=True)
+# question = "What is the capital of France?"
+
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
+# output = model.generate(f"{question}", max_tokens=200)
+# print(output, end="", flush=True)
+with model.chat_session():
+    print(model.generate(f"{question}", max_tokens=1024), flush=True)
